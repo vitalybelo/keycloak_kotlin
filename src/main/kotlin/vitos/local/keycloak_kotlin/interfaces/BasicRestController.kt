0385448a478ce4af53025ac6a58c6ff3f2a65116
BELOTSERKOVSKY, Vitaly A. (Ext. - UniCredit Bank - RUS) <VBelotserkovsky.external@unicredit.ru>
2025-04-16 15:23:20 +0300

    добавлено описание в BasicRestController

--- src/main/kotlin/vitos/local/keycloak_kotlin/interfaces/BasicRestController.kt
+++ src/main/kotlin/vitos/local/keycloak_kotlin/interfaces/BasicRestController.kt
@@ -1,6 +1,11 @@
 package vitos.local.keycloak_kotlin.interfaces
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
@@ -10,6 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping
 interface BasicRestController {
 
 
+    /**
+     * Выполняет проверку авторизации входящего http запроса по протоколу Basic и тестирует метод
+     * формирования заголовка авторизации для исходящих http запросов
+     *
+     * @param headers карта заголовков http запроса
+     * @return сообщение и статус выполнения
+     */
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200", description = "GRANTED - запрос авторизирован", content = [
+                    (Content(
+                        mediaType = "application/json", array = (
+                                ArraySchema(schema = Schema(implementation = Any::class)))
+                    ))]
+            ),
+            ApiResponse(responseCode = "403", description = "Доступ не разрешен", content = [Content()])
+        ]
+    )
     @GetMapping("/auth")
     @Operation(summary = "Имитация запроса с Basic авторизацией в заголовке")
     fun receiveRequestBasicAuthorization(@RequestHeader headers: Map<String, String>?): ResponseEntity<Any>
--- src/main/kotlin/vitos/local/keycloak_kotlin/services/BasicRestService.kt
+++ src/main/kotlin/vitos/local/keycloak_kotlin/services/BasicRestService.kt
@@ -15,14 +15,26 @@ class BasicRestService(
 
     private val log = LoggerFactory.getLogger(BasicRestService::class.java)
 
+
+    /**
+     * Выполняет проверку авторизации входящего http запроса по протоколу Basic и тестирует метод
+     * формирования заголовка авторизации для исходящих http запросов
+     *
+     * @param headers карта заголовков http запроса
+     * @return сообщение и статус выполнения
+     */
     fun getBasicAuthorization(@RequestHeader headers: Map<String, String>?): ResponseEntity<Any> {
 
         if (basicAuthorizationService.isAuthorized(headers)) {
+
             log.info(">>>> Getting basic authorization :: $headers")
+
             val result1 = basicAuthorizationService.addBasicHeader(headers)
             log.info(">>>> Creating basic authorization result1 :: $result1")
+
             val result2 = basicAuthorizationService.addBasicHeader()
             log.info(">>>> Creating basic authorization result2 :: $result2")
+
             return ResponseEntity("GRANTED", HttpStatus.OK)
         }
         return ResponseEntity("Unauthorized request", HttpStatus.UNAUTHORIZED)
